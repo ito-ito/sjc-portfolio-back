b8e372ac01345e26455caffb67883f858037167a
ito-ito <ittooco@gmail.com>
2024-08-22 17:52:21 +0900

    :sparkles: Projectにスキーマ定義を追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,10 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe ProjectsController, type: :request do
   let(:project) { create(:project) }
-  let(:params) { {project: {title: 'title', description: 'description_text', start_date: '2024-04-01'.to_date }}}
+  let(:params) do
+    {
+      project: {
+        title: "title",
+        description: "description_text",
+        start_date: "2024-04-01".to_date
+      }
+    }
+  end
 
-  describe 'GET #index' do
+  describe "GET #index" do
     subject { get "/projects", params: path_parameters }
 
     before { create_list(:project, 10) }
@@ -14,59 +22,66 @@ describe ProjectsController, type: :request do
     it do
       subject
       expect(response).to have_http_status(:success)
+      assert_schema_conform
     end
 
     describe "params limit" do
-      context 'params is nil' do
-        it 'limitを指定しない場合全件返ってくること' do
+      context "params is nil" do
+        it "limitを指定しない場合全件返ってくること" do
           subject
           expect(JSON.parse(response.body).length).to eq 10
+          assert_schema_conform
         end
       end
 
-      context 'limit 3' do
-        let(:path_parameters) { {limit: 3} }
-        it '指定された件数だけ件数だけ返ってくること' do
+      context "limit 3" do
+        let(:path_parameters) { { limit: 3 } }
+        it "指定された件数だけ件数だけ返ってくること" do
           subject
           expect(JSON.parse(response.body).length).to eq 3
+          assert_schema_conform
         end
       end
     end
   end
 
-  describe 'GET #show' do
+  describe "GET #show" do
     subject { get "/projects/#{project.id}" }
 
     it do
       subject
       expect(response).to have_http_status(:success)
+      assert_schema_conform
     end
   end
 
-  describe 'POST #create' do
-    subject { post '/projects', params: params }
+  describe "POST #create" do
+    subject { post "/projects", params: params }
 
     it do
       subject
       expect(response).to have_http_status(:success)
+      assert_schema_conform
     end
   end
 
-  describe 'PUT #update' do
-    subject { put "/projects/#{project.id}", params: params  }
+  describe "PUT #update" do
+    subject { put "/projects/#{project.id}", params: params }
 
     it do
       subject
       expect(response).to have_http_status(:success)
+      assert_schema_conform
     end
   end
 
-  describe 'DELETE #destroy' do
+  describe "DELETE #destroy" do
     subject { delete "/projects/#{project.id}" }
 
     it do
       subject
       expect(response).to have_http_status(:success)
+      assert_schema_conform
     end
   end
 end
